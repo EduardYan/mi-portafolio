08723a067a04376460614fa0d547cd4635f0f167
EduardYan <eduarygp@gmail.com>
2023-12-30 14:17:50 -0600

    First commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,78 @@
+const Tabs = ({ children, route }) => {
+  return (
+    <section className='md:flex'>
+      <ul className='flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0'>
+        <li>
+          <a
+            href='/'
+            className={
+              route === '/'
+                ? 'inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600'
+                : 'inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full da rk:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white'
+            }
+            aria-current='page'
+          >
+            <svg
+              className='w-4 h-4 me-2'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+            >
+              <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
+            </svg>
+            Introducción
+          </a>
+        </li>
+        <li>
+          <a
+            href='/projects'
+            className={
+              route === '/projects'
+                ? 'inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600'
+                : 'inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full da rk:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white'
+            }
+          >
+            <svg
+              className='w-4 h-4 me-2'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              viewBox='0 0 18 18'
+            >
+              <path d='M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z' />
+            </svg>
+            Proyectos
+          </a>
+        </li>
+        <li>
+          <a
+            href='/contact'
+            className={
+              route === '/contact'
+                ? 'inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600'
+                : 'inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full da rk:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white'
+            }
+          >
+            <svg
+              className='w-5 h-5 me-2'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+            >
+              <path
+                fill='currentColor'
+                d='M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863q-2.5-2.5-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3'
+              />
+            </svg>
+            Contacto
+          </a>
+        </li>
+      </ul>
+      <div className='p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full'>
+        {children || ''}
+      </div>
+    </section>
+  )
+}
+
+export default Tabs
